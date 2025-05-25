12fea2b461f2327aeb34e1cca1072fe444bc8d8d
Thomas Lynch <tom@69420.me>
2025-05-25 13:25:23 +1000

    Add verbose logging to be able to add IPs to a map and log challenge and hash verification info

--- src/lua/libs/utils.lua
+++ src/lua/libs/utils.lua
@@ -1,6 +1,7 @@
 local _M = {}
 local sha = require("sha")
 local tor_control_port_password = os.getenv("TOR_CONTROL_PORT_PASSWORD")
+local verbose_log_map = Map.new("/etc/haproxy/map/verbose_log.map", Map._str)
 
 -- get header from different place depending on action vs view
 function _M.get_header_from_context(context, header_name, is_applet)
@@ -33,6 +34,10 @@ function _M.generate_challenge(context, salt, user_key, ddos_config, is_applet)
 	local challenge_hash = sha.sha3_256(salt .. ip .. user_key .. custom_value .. user_agent)
 
 	local expiry = core.now()["sec"] + ddos_config["cex"]
+
+	if verbose_log_map:lookup(ip) ~= nil then
+		print('verbose_log_map utils.generate_challenge: ' .. salt .. ' ' .. ip .. ' ' .. user_key .. ' ' .. custom_value .. ' "' .. user_agent .. '"')
+	end
 
 	return challenge_hash, expiry
 
--- src/lua/scripts/bot-check.lua
+++ src/lua/scripts/bot-check.lua
@@ -66,6 +66,19 @@ end
 
 local css_map = Map.new("/etc/haproxy/map/css.map", Map._str);
 
+local verbose_log_map = Map.new("/etc/haproxy/map/verbose_log.map", Map._str)
+function tableToString(tbl)
+    local result = {}
+    for k, v in pairs(tbl) do
+        if type(v) == "table" then
+            table.insert(result, k .. "=" .. tableToString(v))
+        else
+            table.insert(result, k .. "=" .. tostring(v))
+        end
+    end
+    return "{" .. table.concat(result, ", ") .. "}"
+end
+
 function _M.secondsToDate(seconds)
 	local formattedDate = os.date("!%a, %d-%b-%y %H:%M:%S GMT", seconds)
 	return formattedDate
@@ -526,6 +539,15 @@ end
 function _M.check_captcha_status(txn)
 	local parsed_request_cookies = cookie.get_cookie_table(txn.sf:hdr("Cookie"))
 	local received_captcha_cookie = parsed_request_cookies["_basedflare_captcha"] or ""
+
+	-- verbose logging for specific ip debugging
+	local verbose_log = verbose_log_map:lookup(txn.sf:src()) ~= nil
+	if verbose_log then
+		print('verbose_log_map bot-check.check_captcha_status: ' .. txn.sf:hdr("Cookie"))
+		print('verbose_log_map bot-check.check_captcha_status: ' .. tableToString(parsed_request_cookies))
+		print('verbose_log_map bot-check.check_captcha_status: ' .. received_captcha_cookie or "NO_CAPTCHA_COOKIE")
+	end
+
 	-- split the cookie up
 	local split_cookie = utils.split(received_captcha_cookie, "#")
 	if #split_cookie ~= 4 then
@@ -539,12 +561,18 @@ function _M.check_captcha_status(txn)
 	-- expiry check
 	local number_expiry = tonumber(given_expiry, 10)
 	if number_expiry == nil or number_expiry <= core.now()['sec'] then
+		if verbose_log then
+			print('verbose_log_map bot-check.check_captcha_status: invalid expiry')
+		end
 		return
 	end
 	-- regenerate the user hash and compare it
 	local ddos_config = _M.get_ddos_config(txn, false)
 	local generated_user_hash = utils.generate_challenge(txn, captcha_cookie_secret, given_user_key, ddos_config, false)
 	if generated_user_hash ~= given_user_hash then
+		if verbose_log then
+			print('verbose_log_map bot-check.check_captcha_status: mismatched challenge hash')
+		end
 		return
 	end
 	-- regenerate the signature and compare it
@@ -553,12 +581,26 @@ function _M.check_captcha_status(txn)
 	if given_signature == generated_signature then
 		return txn:set_var("txn.captcha_passed", true)
 	end
+
+	if verbose_log then
+		print('verbose_log_map bot-check.check_captcha_status: mismatched signature')
+	end
+
 end
 
 -- check if pow cookie is valid
 function _M.check_pow_status(txn)
 	local parsed_request_cookies = cookie.get_cookie_table(txn.sf:hdr("Cookie"))
 	local received_pow_cookie = parsed_request_cookies["_basedflare_pow"] or ""
+
+	-- verbose logging for specific ip debugging
+	local verbose_log = verbose_log_map:lookup(txn.sf:src()) ~= nil
+	if verbose_log then
+		print('verbose_log_map bot-check.check_pow_status: ' .. txn.sf:hdr("Cookie"))
+		print('verbose_log_map bot-check.check_pow_status: ' .. tableToString(parsed_request_cookies))
+		print('verbose_log_map bot-check.check_pow_status: ' .. received_pow_cookie or "NO_POW_COOKIE")
+	end
+
 	-- split the cookie up
 	local split_cookie = utils.split(received_pow_cookie, "#")
 	if #split_cookie ~= 5 then
@@ -573,12 +615,18 @@ function _M.check_pow_status(txn)
 	-- expiry check
 	local number_expiry = tonumber(given_expiry, 10)
 	if number_expiry == nil or number_expiry <= core.now()['sec'] then
+		if verbose_log then
+			print('verbose_log_map bot-check.check_pow_status: invalid expiry')
+		end
 		return
 	end
 	-- regenerate the challenge and compare it
 	local ddos_config = _M.get_ddos_config(txn, false)
 	local generated_challenge_hash = utils.generate_challenge(txn, pow_cookie_secret, given_user_key, ddos_config, false)
 	if given_challenge_hash ~= generated_challenge_hash then
+		if verbose_log then
+			print('verbose_log_map bot-check.check_pow_status: mismatched challenge hash')
+		end
 		return
 	end
 	-- regenerate the signature and compare it
@@ -587,6 +635,11 @@ function _M.check_pow_status(txn)
 	if given_signature == generated_signature then
 		return txn:set_var("txn.pow_passed", true)
 	end
+
+	if verbose_log then
+		print('verbose_log_map bot-check.check_pow_status: mismatched signature')
+	end
+
 end
 
 return _M
