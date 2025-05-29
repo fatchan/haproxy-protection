d11bc7ebcf2c3d72ad8c4eabb3dc0c0766d6d7b7
Thomas Lynch <tom@69420.me>
2025-05-29 18:41:51 +1000

    Refactor get_server_names/get_server_ip

--- src/lua/scripts/register-bot-check.lua
+++ src/lua/scripts/register-bot-check.lua
@@ -8,77 +8,55 @@ local backends_map = Map.new('/etc/haproxy/map/backends.map', Map._str)
 local haproxy_cn = os.getenv("HAPROXY_CONTINENT") or "XX" -- should never be XX but avoid typing issue
 
 
-function Get_server_ip(txn)
+function Get_server_info(txn, return_ip)
     local key = txn.sf:hdr("Host")
     local target_backend_cn = haproxy_cn
     local value = backends_map:lookup(key or "")
-    -- print("fc_dst_port: " .. txn:get_var("txn.cport"))
+    
     if value ~= nil then
         local filtered_backends = {}
         local all_backends = {}
         local vals = utils.split(value, ",")
+        
         -- Single pass to filter and collect backends
         for _, backend in ipairs(vals) do
             local backend_server_name, backend_cn = backend:match(server_cn_split_regex)
-            -- local server_up = txn.f:srv_is_up('servers/' .. backend_server_name)
-            -- print('backend_server_name: ' .. backend_server_name)
-            -- print('server_up: ' .. server_up)
-            if backend_server_name then -- and server_up == 1 then
+            if backend_server_name then
                 table.insert(all_backends, backend_server_name)
                 if backend_cn == target_backend_cn then
                     table.insert(filtered_backends, backend_server_name)
                 end
             end
         end
+        
         -- Randomly select from filtered backends if available
+        local selected_backend
         if #filtered_backends > 0 then
-            local s = filtered_backends[math.random(#filtered_backends)]
-            local s_ip, _ = core.proxies["servers"]["servers"][s]:get_addr():match(server_port_split_pattern)
-            return s_ip
+            selected_backend = filtered_backends[math.random(#filtered_backends)]
         elseif #all_backends > 0 then
             -- If no filtered backends, randomly select from all backends
-            local s = all_backends[math.random(#all_backends)]
-            local s_ip, _ = core.proxies["servers"]["servers"][s]:get_addr():match(server_port_split_pattern)
-            return s_ip
+            selected_backend = all_backends[math.random(#all_backends)]
+        end
+        
+        if selected_backend then
+            if return_ip then
+                -- todo: more ifs here?
+                local s_ip, _ = core.proxies["servers"]["servers"][selected_backend]:get_addr():match(server_port_split_pattern)
+                return s_ip
+            else
+                return selected_backend
+            end
         end
     end
     return ""
 end
 
+function Get_server_ip(txn)
+    return Get_server_info(txn, true)
+end
 
 function Get_server_names(txn)
-    local key = txn.sf:hdr("Host")
-    local target_backend_cn = haproxy_cn
-    local value = backends_map:lookup(key or "")
-    -- print("fc_dst_port: " .. txn:get_var("txn.cport"))
-    if value ~= nil then
-        local filtered_backends = {}
-        local all_backends = {}
-        local vals = utils.split(value, ",")
-        -- Single pass to filter and collect backends
-        for _, backend in ipairs(vals) do
-            local backend_server_name, backend_cn = backend:match(server_cn_split_regex)
-            -- local server_up = txn.f:srv_is_up('servers/' .. backend_server_name)
-            -- print('backend_server_name: ' .. backend_server_name)
-            -- print('server_up: ' .. server_up)
-            if backend_server_name then -- and server_up == 1 then
-                table.insert(all_backends, backend_server_name)
-                if backend_cn == target_backend_cn then
-                    table.insert(filtered_backends, backend_server_name)
-                end
-            end
-        end
-        -- Randomly select from filtered backends if available
-        if #filtered_backends > 0 then
-            local s = filtered_backends[math.random(#filtered_backends)]
-            return s
-        elseif #all_backends > 0 then
-            -- If no filtered backends, randomly select from all backends
-            local s = all_backends[math.random(#all_backends)]
-            return s
-        end
-    end
-    return ""
+    return Get_server_info(txn, false)
 end
 
 core.register_fetches("get_server_names", Get_server_names)
